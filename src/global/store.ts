31b23e738ec3de1807b5c0490ab34639c5f83537
Barry Jacobs <barry.jacobs@mechenet.com>
2022-11-06 13:31:01 +0000

    Added valtio linting and devtools

--- src/global/store.ts
+++ src/global/store.ts
@@ -1,5 +1,6 @@
 import { proxy } from "valtio"
 import { v4 as uuidv4 } from "uuid"
+import { devtools } from "valtio/utils"
 
 interface Store {
   bearerToken: string
@@ -8,5 +9,7 @@ interface Store {
 const store = proxy<Store>({
   bearerToken: uuidv4()
 })
+
+devtools(store)
 
 export default store
